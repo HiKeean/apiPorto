21762d6873ef1c0c2822ffd44b04c82c795ff7e2
hikeean <hizkiaalbertianhal@gmail.com>
2025-01-13 10:44:46 +0700

    abtme.js

--- app/models/abtme.js
+++ app/models/abtme.js
@@ -3,7 +3,7 @@ const db = require('./index'); // Mengimpor koneksi database dari index.js
 
 async function getAbt() {
     try {
-        const [rows] = await db.promise().query('SELECT * FROM abtMe');
+        const [rows] = await db.query('SELECT * FROM abtMe');
         return rows; 
     } catch (error) {
         console.error('Error fetching profiles:', error);
